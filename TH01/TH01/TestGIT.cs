f81500c6e965d398c4f512b25580d33bf566d00a
ttoann <ntt26.5.2004@gmail.com>
2024-01-25 14:18:57 +0700

    test commit 1

---
+++ TH01/TH01/TestGIT.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH01
+{
+    internal class TestGIT
+    {
+        public string name;
+        public string version;
+        public string description;
+    }
+}
